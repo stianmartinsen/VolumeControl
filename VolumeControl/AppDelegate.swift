5ef10969b9fd603f7fa8146565eaa200cdd2f74d
Stian Martinsen <stianm@me.com>
2015-06-10 20:53:04 +0200

    Initial Commit

---
+++ VolumeControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VolumeControl
+//
+//  Created by Stian Martinsen on 10.06.15.
+//  Copyright (c) 2015 Stian Martinsen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
